0cb2ef690bcba4dbfe7e67c1c3d529bea04b0fd8
Hameer Abbasi <hameerabbasi@yahoo.com>
2019-03-04 14:38:14 +0100

    Change so that mtypes are first-class types and not emulated.

--- mtypes/_mtypes.py
+++ mtypes/_mtypes.py
@@ -1,44 +1,45 @@
 from ndtypes import ndt
 from xnd import xnd
 
-__all__ = ['mtype']
+__all__ = ['Xnd', 'mtype']
 
-
-class _mclass(type):
+class _XndMeta(type):
+    def __call__(self, *args, type=None, **kwargs):
+        if type is None:
+            raise ValueError('type must be provided.')
+            
+        return mtype(type)(*args, **kwargs)
+    
     def __instancecheck__(self, instance):
-        return isinstance(self, ndt)
+        return isinstance(type(instance), mtype)
 
     def __subclasscheck__(self, subclass):
-            return issubclass(subclass, ndt) or isinstance(subclass, _mclass)
+        return isinstance(subclass, mtype)
+        
+
+class Xnd(metaclass=_XndMeta):
+    pass
 
 
-class mtype(ndt, metaclass=_mclass):
-    def __new__(cls, *args, **kwargs):
-        return ndt(*args, **kwargs)
+class mtype(type):
+    def __new__(cls, t: str):
+        t = ndt(t)
 
-    def __class_getitem__(cls, key):
-        key = ndt(key)
-
-        class mclass_specific(_mclass):
-            ndt = key
-
-            def __instancecheck__(self, instance):
-                return isinstance(instance, xnd) and instance.type == self.ndt
-
-            def __subclasscheck__(self, subclass):
-                return self.ndt == subclass.ndt
-
-            def __eq__(self, other):
-                return self.ndt == other.ndt
-
-            def __neq__(self, other):
-                return self.ndt != other.ndt
+        class XndSpecific:
+            def __init__(self, *args, **kwargs):
+                self.xnd = xnd(*args, type=t, **kwargs)
 
             def __getattr__(self, key):
-                return getattr(self.ndt, key)
+                getattr(xnd, key)
 
-        class NdtSpecific(xnd, metaclass=mclass_specific):
-            def __new__(cls, *args, **kwargs):
-                return xnd(*args, **kwargs, type=key)
+        name = f"{cls.__name__}({str(t)})"
+        return super().__new__(cls, name, XndSpecific.__bases__, {**XndSpecific.__dict__, '__module__': __name__, '__qualname__': name, 'ndt': t})
 
-        return NdtSpecific
+    def __instancecheck__(self, instance):
+        return isinstance(type(instance), type(self)) and self.ndt == type(instance).ndt
+
+    def __subclasscheck__(self, subclass):
+        return isinstance(subclass, type(self)) and self.ndt == subclass.ndt
+
+    def __getattr__(self, key):
+        return getattr(self.ndt, key)
--- test/test_mtypes.py
+++ test/test_mtypes.py
@@ -5,7 +5,7 @@ import pytest
 from ndtypes import ndt
 from xnd import xnd
 
-from mtypes import mtype
+from mtypes import mtype, Xnd
 
 
 def chain_marks(*marks):
@@ -28,6 +28,7 @@ mark_output_types = pytest.mark.parametrize('output_type', [
 
 mark_both_types = chain_marks(mark_input_types, mark_output_types)
 
+
 def value_of_shape(shape):
     value = 0
 
@@ -36,16 +37,18 @@ def value_of_shape(shape):
 
     return value
 
-def xnd_of_type(ndt_type):
+
+def Xnd_of_type(ndt_type):
     shape = ndt(ndt_type).shape
     value = value_of_shape(shape)
-    return xnd(value, type=ndt_type)
+    return Xnd(value, type=ndt_type)
+
 
 @mark_both_types
 def test_mtypes(input_type, output_type):
     expected = ndt(input_type) == ndt(output_type)
 
-    assert expected == isinstance(xnd_of_type(output_type), mtype[input_type])
+    assert expected == isinstance(Xnd_of_type(output_type), mtype(input_type))
 
 
 @mark_both_types
@@ -53,15 +56,15 @@ def test_subclass(input_type, output_type):
     expected = ndt(input_type) == ndt(output_type)
 
     assert expected == issubclass(
-        mtype[input_type], mtype[output_type])
+        mtype(input_type), mtype(output_type))
 
 
 @mark_input_types
 def test_subclass_from_generic(input_type):
-    assert issubclass(mtype[input_type], mtype)
+    assert issubclass(mtype(input_type), Xnd)
 
 @mark_input_types
 def test_creation(input_type):
-    input_mtype = mtype[input_type]
-    value = value_of_shape(input_mtype.shape)
-    assert isinstance(input_mtype(value), mtype[input_type])
+    input_mtype=mtype(input_type)
+    value=value_of_shape(input_mtype.shape)
+    assert isinstance(input_mtype(value), mtype(input_type))
